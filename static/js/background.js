59a9baf1063a484dec7e7818679b926a7fbb6a0f
4xtc <44xtc44@gmail.com>
2024-08-06 21:12:25 +0200

    runs on manifest v3

--- static/js/background.js
+++ static/js/background.js
@@ -2,15 +2,14 @@
 "use strict";
 /**
  * Can load either "background" or "content_scripts" by pressing button
+ * https://stackoverflow.com/questions/69296754/chrome-extension-action-onclicked
+ * Called when the user clicks on the browser action.
  */
-function openTab(){
-    var newTab = browser.tabs.create({ 
-      url: "/static/addon.html",
-      active:true
-    }).then(() => {
-      browser.tabs.executeScript({
-        code: `console.log('Add-on creator:', '44xtc44');`,
-      });
+chrome.action.onClicked.addListener(tab => {
+  // Send a message to the active tab
+  chrome.tabs.create({ url: "/static/addon.html" }).then(() => {
+    chrome.tabs.executeScript({
+      code: `console.log('Add-on creator:', '44xtc44');`,
     });
-}
-browser.browserAction.onClicked.addListener(openTab)
+  });
+});
